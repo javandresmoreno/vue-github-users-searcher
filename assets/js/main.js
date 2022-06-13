5e5c2796a84259478fa4e267a0f7f922a36fc88e
Javier Moreno <javier@solido.co>
2022-06-12 20:46:06 -0500

    Finaliza creación de petición a API, captura de errores y utilización de renderizado condicional

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,42 @@ const API = "https://api.github.com/users/";
 
 // Function to search users at Github API
 
-async function doSearch() {
-    const response = await fetch(API + "javandresmoreno")
-    const data = await response.json()
-    console.log(data)
-}
+// async function doSearch() {
+//     const response = await fetch(API + "javandresmoreno")
+//     const data = await response.json()
+//     console.log(data)
+// }
 
 
 const app = Vue.createApp({
     data() {
         return {
-            message: "Juan Nuñez's - VueJS 3 Course"
+            subtitle: "Juan Nuñez's - VueJS 3 Course",
+            search: null,
+            result: null,
+            error: null, 
+        }
+    }, 
+    methods: {
+        async doSearch() {
+            //Con la linea siguiente reseteo mis resultados
+            this.result = this.error = null 
+
+            try {
+                const response = await fetch(API + this.search)
+                if(!response.ok) throw new Error("Sorry, user not found. 😓")
+                console.log(response)
+
+                const data = await response.json()
+                console.log(data)
+
+                this.result = true
+                
+            } catch (error) {
+                this.error = error
+            } finally {
+                this.search = null 
+            }
         }
     }
 })
